0a60cc0b187a38a10ebce506c9f00cb648638e7f
Eduardo Pineda <eduardo.pineda@hpe.com>
2018-02-28 09:43:30 -0600

    Adding mongo-sql conversion

--- assets/src/components/CodeMirrorPanel.js
+++ assets/src/components/CodeMirrorPanel.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 import CodeMirror from 'react-codemirror';
+import Split from 'grommet/components/Split';
 import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/markdown/markdown';
+import './_codeMirrorPanel.css';
+
+var builder = require('mongo-sql');
 
 export default class CodeMirrorPanel extends Component {
 
     constructor() {
         super();
         this.state = {
-            name: 'moSQL Try it',
-            code: '// Code'
+            codeLeft: '// Code',
+            codeRight: '// Result',
+            mode: 'javascript'
         };
     }
 
     updateCode(newCode) {
         this.setState({
-            code: newCode
+            codeLeft: newCode
         });
+
+        
+        var usersQuery = {
+            type: 'select'
+          , table: 'users'
+          , where: { $or: { id: 5, name: 'Bob' } }
+          };
+          const result = builder.sql(usersQuery);
+        console.log(this.state.codeLeft);
     }
 
     render(){
-        let options = {
-            lineNumbers: true
+        const optionsLeft = {
+            lineNumbers: true,
+            mode: this.state.mode
         };
+        const optionsRight = {
+            lineNumbers: true,
+            mode: 'markdown',
+            readOnly: true
+        }
         return (
-            <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this)} options={options} />
+            <Split>
+                <CodeMirror value={this.state.codeLeft} onChange={this.updateCode.bind(this)} options={optionsLeft} />
+                <CodeMirror value={this.state.codeRight} options={optionsRight} />
+            </Split>
+            
         );
     }
 }
