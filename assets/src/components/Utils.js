fea6ec8f9f43babd9f7c126b39eb97c9ed933028
Eduardo Pineda <eduardo.pineda@hpe.com>
2018-03-05 13:03:21 -0600

    Function eval for JSON parsing

--- assets/src/components/Utils.js
+++ assets/src/components/Utils.js
@@ -1,7 +1,12 @@
 const builder = require('mongo-sql');
 
+function looseJsonParse(jsString) {
+  return Function(`"use strict";return ( ${jsString} );`)();
+}
+
 export default function convertMongoToSql(mongoString) {
-  const jsCode = JSON.parse(mongoString.trim());
+  const enclosingText = mongoString.trim().match(/\{([^)]+)\}/)[0];
+  const jsCode = looseJsonParse(enclosingText);
   const result = builder.sql(jsCode);
   return result.toString();
 }
